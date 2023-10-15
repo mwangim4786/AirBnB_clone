0b9b7fa586a525cea784a1dfc3b0818bdb3d1993
Martin Kihungi <mwangi4786@gmail.com>
2023-10-15 10:01:40 -0700

    Create BaseModel from dictionary

--- test_base_model.py
+++ test_base_model.py
@@ -1,14 +1,58 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""
+Defines a class BaseModel
+"""
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Representation of BaseModel"""
+    def __init__(self, *args, **kwargs):
+        """Initialization of a Base instance.
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        
+
+
+    def __str__(self):
+        """prints human readable string representation of [<class name>] (<self.id>) <self.__dict__>"""
+        
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+    
+    def save(self):
+        """Updates the updated_at attribute
+        with the current datetime."""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return the dictionary of the base model instance
+        including __class__ representing the class name.
+        """
+        my_dict = self.__dict__.copy()
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        my_dict["__class__"] = type(self).__name__
+        return my_dict
+
